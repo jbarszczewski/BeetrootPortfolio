5f354ddee4babf6e15c9e1a842098f31f0b411f2
Jakub Barszczewski <jakub.barszczewski@cr-platform.com>
2017-03-23 09:35:08 +0000

    Removed unnecessary imports.

--- wwwroot/dist/main-client.js
+++ wwwroot/dist/main-client.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "3f82c195c41f5a27d624"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6abfba44953f79297b83"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
